699815eb38def1fa5b901e748b46ceb4627c76fb
Matthew Rothenberg <mrothenberg@gmail.com>
2016-08-26 19:26:38 -0400

    no need to do graphite logging
    gets rid of a whole bunch of config boilerplate

--- lib/config.rb
+++ lib/config.rb
@@ -1,43 +1,4 @@
 require 'redis'
-require 'socket'
-
-#convenience method for reading booleans from env vars
-def to_boolean(s)
-  s and !!s.match(/^(true|t|yes|y|1)$/i)
-end
-
-# verbose mode or no
-VERBOSE = to_boolean(ENV["VERBOSE"]) || false
 
 # db setup -- checks for REDIS_URL by default, or fallback to localhost
 REDIS = Redis.new()
-
-# environment checks
-def is_production?
-  ENV["RACK_ENV"] == 'production'
-end
-
-def is_development?
-  ENV["RACK_ENV"] == 'development'
-end
-
-def is_development_frontend_only?
-  is_development? && to_boolean(ENV["FRONTEND_ONLY"])
-end
-
-# configure logging to graphite in production
-def graphite_log(metric, count)
-  @hostedgraphite_apikey = ENV['HOSTEDGRAPHITE_APIKEY']
-  # puts "Graphite log - #{metric}: #{count}" if VERBOSE
-  if is_production?
-    sock = UDPSocket.new
-    sock.send @hostedgraphite_apikey + ".#{metric} #{count}\n", 0, "carbon.hostedgraphite.com", 2003
-  end
-end
-
-# same as above but include heroku dyno hostname
-def graphite_dyno_log(metric,count)
-  dyno = ENV['DYNO'] || 'unknown-host'
-  metric_name = "#{dyno}.#{metric}"
-  graphite_log metric_name, count
-end
